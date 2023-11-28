5402250de5054ed36c3668aace38a517a8b60951
Mike Goldenberg <csrutil@tuta.io>
2023-11-28 14:38:30 +0100

    libs and utils

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'UkrBuildParis',
+  title: 'Board for high quality services',
+  description: 'A board for renovation and repair services based in Paris',
+  locale: 'en',
+};
